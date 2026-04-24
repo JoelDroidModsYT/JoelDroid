405acc07f1e97839e5c9982cde35f6d915b5bff4
JoelDroidModsYT <gamerssimulacionjl@gmail.com>
2026-04-24 12:47:47 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10800647
+    "zoneId": 10921026
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
